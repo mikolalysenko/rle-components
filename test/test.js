3de7acc40ab3313671919ad325e0d3dd8a30fe14
Mikola Lysenko <mikolalysenko@gmail.com>
2013-02-15 11:08:17 -0600

    fixed test

--- test/test.js
+++ test/test.js
@@ -1,7 +1,6 @@
-var core        = require("rle-core")
-  , components    = require("../components.js");
+var components    = require("../components.js");
 
-var shape = core.sampleSolid([-32, -32, -32], [32, 32, 32], function(x) {
+var shape = require("rle-sample").solid.dense([-32, -32, -32], [32, 32, 32], function(x) {
   var s = Math.sin(x[0]) + Math.sin(x[1]) + Math.sin(x[2]);
   var b = Math.max(Math.abs(x[0]), Math.abs(x[1]), Math.abs(x[2]));
   return Math.max(s + 2.2, b - 9.9);
